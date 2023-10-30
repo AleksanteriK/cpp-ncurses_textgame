166613ec1c46c2fafe3e31637ec391f466930427
Aleksanteri <juho.koivisto00@outlook.com>
2023-10-30 16:55:18 +0200

    renamed window func src files

---
+++ gamewindows.cpp
@@ -0,0 +1,44 @@
+#include "gamewindows.h"
+
+#define MIDDLE_Y_AXIS (LINES/2)
+#define MIDDLE_X_AXIS (COLS/2)
+
+WINDOW *create_middle_pond(int height, int width, int starty, int startx) {
+    WINDOW *local_win;
+    local_win = newwin(height, width, starty, startx);
+    wbkgd(local_win, COLOR_PAIR(7));
+    wrefresh(local_win);
+    return local_win;
+}
+
+WINDOW *cr_rightsideof_pond(int height, int width, int starty, int startx) {
+    WINDOW *local_win;
+    local_win = newwin(height, width, starty, startx);
+    wbkgd(local_win, COLOR_PAIR(7));
+    wrefresh(local_win);
+    return local_win;
+}
+
+WINDOW *cr_leftsideof_pond(int height, int width, int starty, int startx) {
+    WINDOW *local_win;
+    local_win = newwin(height, width, starty, startx);
+    wbkgd(local_win, COLOR_PAIR(7));
+    wrefresh(local_win);
+    return local_win;
+}
+
+WINDOW *cr_parktrees(int height, int width, int starty, int startx) {
+    WINDOW *local_win;
+    local_win = newwin(height, width, starty, startx);
+    wbkgd(local_win, COLOR_PAIR(8));
+    wrefresh(local_win);
+    return local_win;
+}
+
+WINDOW *cr_eskers(int height, int width, int starty, int startx) {
+    WINDOW *local_win;
+    local_win = newwin(height, width, starty, startx);
+    wbkgd(local_win, COLOR_PAIR(8));
+    wrefresh(local_win);
+    return local_win;
+}
