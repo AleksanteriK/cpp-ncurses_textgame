be1778ec1a79567d5e548511c0359d470eb9f555
Aleksanteri <juho.koivisto00@outlook.com>
2023-12-03 17:41:49 +0200

    forgot to add these

---
+++ item.cpp
@@ -0,0 +1,5 @@
+#include "item.h"
+
+Item::Item(const std::string& itemName) : name(itemName) {
+
+}
---
+++ item.h
@@ -0,0 +1,13 @@
+#ifndef ITEM_H
+#define ITEM_H
+
+#include <iostream>
+
+class Item {
+    public:
+    std::string name;
+
+    Item(const std::string& itemName);
+};
+
+#endif
